3e78c5c6331f17875592af23cc4d2b4c0f517487
David Wörgötter <david.woergoetter@student.tugraz.at>
2018-05-09 23:07:20 +0200

    [DW] REMOVED duplicate function

--- app/src/main/java/me/finance/finance/DatabaseHandler.java
+++ app/src/main/java/me/finance/finance/DatabaseHandler.java
@@ -318,24 +318,6 @@ public class DatabaseHandler{
         return intakes;
     }
 
-    public ArrayList<Permanent> getPermanents()
-    {
-        ArrayList<Permanent> standing_orders = new ArrayList<>();
-        try {
-            Cursor cursorIntakes = database.rawQuery("SELECT * FROM permanents", null);
-            if (cursorIntakes.moveToFirst()) {
-                do {
-                    standing_orders.add(new Permanent(cursorIntakes.getInt(0), cursorIntakes.getFloat(1), cursorIntakes.getString(2), cursorIntakes.getString(3),
-                            cursorIntakes.getString(4), cursorIntakes.getString(5), cursorIntakes.getString(6), cursorIntakes.getInt(7), cursorIntakes.getInt(8), cursorIntakes.getString(9)));
-                } while (cursorIntakes.moveToNext());
-            }
-            cursorIntakes.close();
-        } catch(Exception e) {
-            System.out.println("");
-        }
-        return standing_orders;
-    }
-
     public long addIntake(Intake intake)
     {
         SQLiteStatement sql = database.compileStatement("INSERT INTO intakes (_id, value, date, name, comment, category, payment_opt) VALUES (NULL, ?, ?, ?,?,?,? )");
