e28019af340dec998e27332897313793606c4969
Felix Auf <felix.auf@student.tugraz.at>
2018-05-23 16:37:49 +0200

    [FA, AW] Manage Categories and Accounts implemented

--- app/src/main/java/me/finance/finance/DatabaseHandler.java
+++ app/src/main/java/me/finance/finance/DatabaseHandler.java
@@ -19,7 +19,7 @@ import me.finance.finance.Model.Intake;
 import me.finance.finance.Model.Payment;
 import me.finance.finance.Model.Permanent;
 
-public class DatabaseHandler{
+public class DatabaseHandler {
 
     private SQLiteOpenHelper openHelper;
     private SQLiteDatabase database;
@@ -39,7 +39,7 @@ public class DatabaseHandler{
         return instance;
     }
 
-     /*
+    /*
      *
      * Establishes connection to database
      *
@@ -49,17 +49,17 @@ public class DatabaseHandler{
     }
 
     /*
-    *
-    * Closes connection if exists
-    *
-    */
+     *
+     * Closes connection if exists
+     *
+     */
     public void close() {
         if (database != null) {
             this.database.close();
         }
     }
 
-     /*
+    /*
      *
      * Creation of tables for Database:
      * intakes (_id (int), value(float), date(date), name(text), comment(text), category(int), payment_opt(int))
@@ -112,7 +112,7 @@ public class DatabaseHandler{
         Cursor cursor = database.rawQuery("SELECT * FROM categories", null);
         cursor.moveToFirst();
         while (!cursor.isAfterLast()) {
-            categories.add(new Category(cursor.getInt(0),cursor.getString(1)));
+            categories.add(new Category(cursor.getInt(0), cursor.getString(1)));
             cursor.moveToNext();
         }
         cursor.close();
@@ -134,7 +134,7 @@ public class DatabaseHandler{
         Cursor cursor = database.rawQuery("SELECT * FROM payment", null);
         cursor.moveToFirst();
         while (!cursor.isAfterLast()) {
-            payments.add(new Payment(cursor.getInt(0),cursor.getString(1)));
+            payments.add(new Payment(cursor.getInt(0), cursor.getString(1)));
             cursor.moveToNext();
         }
         cursor.close();
@@ -151,16 +151,16 @@ public class DatabaseHandler{
         return payment;
     }
 
-    public void removeCategory(String name){
+    public void removeCategory(String name) {
         database.delete("categories", "name = '" + name + "'", null);
     }
 
-    public void removePayment(String name){
+    public void removePayment(String name) {
         database.delete("payment", "name = '" + name + "'", null);
     }
 
 
-    public void deleteTableContents(){
+    public void deleteTableContents() {
         database.delete("intakes", null, null);
         database.delete("permanents", null, null);
         database.delete("categories", null, null);
@@ -171,61 +171,75 @@ public class DatabaseHandler{
         database.delete("intakes", null, null);
     }
 
-    public boolean updateIntakes(Intake... intakes){
-        for(Intake intake : intakes){
+    public boolean updateIntakes(Intake... intakes) {
+        for (Intake intake : intakes) {
             ContentValues values = new ContentValues();
-            values.put("value",intake.getValue());
-            values.put("date",intake.getDateFormatted());
-            values.put("name",intake.getName());
-            values.put("comment",intake.getComment());
-            values.put("category",intake.getCategory());
-            values.put("payment_opt",intake.getPayment_opt());
+            values.put("value", intake.getValue());
+            values.put("date", intake.getDateFormatted());
+            values.put("name", intake.getName());
+            values.put("comment", intake.getComment());
+            values.put("category", intake.getCategory());
+            values.put("payment_opt", intake.getPayment_opt());
 
-            if(database.update("intakes",values,"_id = ?", new String[]{String.valueOf(intake.getId())}) != 1){
+            if (database.update("intakes", values, "_id = ?", new String[]{String.valueOf(intake.getId())}) != 1) {
                 return false;
             }
         }
         return true;
     }
 
-    public boolean updateCategories(Category... categories){
-        for(Category category : categories){
+    public boolean updateCategories(Category... categories) {
+        for (Category category : categories) {
             ContentValues values = new ContentValues();
-            values.put("name",category.getName());
+            values.put("name", category.getName());
 
-            if(database.update("categories",values,"_id = ?", new String[]{String.valueOf(category.getId())}) != 1){
+            if (database.update("categories", values, "_id = ?", new String[]{String.valueOf(category.getId())}) != 1) {
                 return false;
             }
         }
         return true;
     }
 
-    public boolean updatePayment(Payment... payments){
-        for(Payment payment : payments){
+    public boolean updatePayment(Payment... payments) {
+        for (Payment payment : payments) {
             ContentValues values = new ContentValues();
-            values.put("name",payment.getName());
+            values.put("name", payment.getName());
 
-            if(database.update("categories",values,"_id = ?", new String[]{String.valueOf(payment.getId())}) != 1){
+            if (database.update("payment", values, "_id = ?", new String[]{String.valueOf(payment.getId())}) != 1) {
                 return false;
             }
         }
         return true;
     }
 
-    public boolean updatePermanent(Permanent... permanents){
-        for(Permanent permanent : permanents){
-            ContentValues values = new ContentValues();
-            values.put("value",permanent.getValue());
-            values.put("start_date",permanent.getStartDate());
-            values.put("iteration",permanent.getIteration());
-            values.put("end_date",permanent.getEndDate());
-            values.put("name",permanent.getName());
-            values.put("comment",permanent.getComment());
-            values.put("category",permanent.getCategory());
-            values.put("payment_opt",permanent.getPayment_opt());
-            values.put("next_exec",permanent.getNext_exec());
+    public boolean updatePayment(String new_name, String old_name){
+        ContentValues values = new ContentValues();
+        values.put("name", new_name);
+        database.update("payment", values, "name = ?", new String[] { old_name });
+        return true;
+    }
 
-            if(database.update("categories",values,"_id = ?", new String[]{String.valueOf(permanent.getId())}) != 1){
+    public boolean updateCategories(String new_name, String old_name){
+        ContentValues values = new ContentValues();
+        values.put("name", new_name);
+        database.update("categories", values, "name = ?", new String[] { old_name });
+        return true;
+    }
+
+    public boolean updatePermanent(Permanent... permanents) {
+        for (Permanent permanent : permanents) {
+            ContentValues values = new ContentValues();
+            values.put("value", permanent.getValue());
+            values.put("start_date", permanent.getStartDate());
+            values.put("iteration", permanent.getIteration());
+            values.put("end_date", permanent.getEndDate());
+            values.put("name", permanent.getName());
+            values.put("comment", permanent.getComment());
+            values.put("category", permanent.getCategory());
+            values.put("payment_opt", permanent.getPayment_opt());
+            values.put("next_exec", permanent.getNext_exec());
+
+            if (database.update("categories", values, "_id = ?", new String[]{String.valueOf(permanent.getId())}) != 1) {
                 return false;
             }
         }
@@ -233,16 +247,12 @@ public class DatabaseHandler{
     }
 
 
-
-
-    public Intake getIntake(int id)
-    {
+    public Intake getIntake(int id) {
         Intake intake = null;
         Cursor cursor = database.rawQuery("SELECT * FROM intakes WHERE _id = ?", new String[]{String.valueOf(id)});
-        if(cursor.moveToFirst())
-        {
+        if (cursor.moveToFirst()) {
             if (cursor.isNull(5)) {
-                intake = new Intake(cursor.getInt(0), cursor.getDouble(1), cursor.getString(2), cursor.getString(3),cursor.getString(4), null, cursor.getInt(6));
+                intake = new Intake(cursor.getInt(0), cursor.getDouble(1), cursor.getString(2), cursor.getString(3), cursor.getString(4), null, cursor.getInt(6));
             } else {
                 intake = new Intake(cursor.getInt(0), cursor.getDouble(1), cursor.getString(2), cursor.getString(3), cursor.getString(4), cursor.getInt(5), cursor.getInt(6));
             }
@@ -251,40 +261,35 @@ public class DatabaseHandler{
         return intake;
     }
 
-    public ArrayList<Intake> getPositiveIntakes()
-    {
+    public ArrayList<Intake> getPositiveIntakes() {
         ArrayList<Intake> intakes = new ArrayList<>();
         Cursor cursor = database.rawQuery("SELECT * FROM intakes", null);
-        if(cursor.moveToFirst())
-        {
+        if (cursor.moveToFirst()) {
             do {
-                if(cursor.getFloat(1) > 0)
-                  intakes.add(new Intake(cursor.getInt(0), cursor.getFloat(1), cursor.getString(2), cursor.getString(3),cursor.getString(4),cursor.getInt(5),cursor.getInt(6)));
+                if (cursor.getFloat(1) > 0)
+                    intakes.add(new Intake(cursor.getInt(0), cursor.getFloat(1), cursor.getString(2), cursor.getString(3), cursor.getString(4), cursor.getInt(5), cursor.getInt(6)));
 
-            }while(cursor.moveToNext());
+            } while (cursor.moveToNext());
         }
         cursor.close();
         return intakes;
     }
 
-    public ArrayList<Intake> getNegativeIntakes()
-    {
+    public ArrayList<Intake> getNegativeIntakes() {
         ArrayList<Intake> intakes = new ArrayList<>();
         Cursor cursor = database.rawQuery("SELECT * FROM intakes", null);
-        if(cursor.moveToFirst())
-        {
+        if (cursor.moveToFirst()) {
             do {
-                if(cursor.getFloat(1) < 0)
-                    intakes.add(new Intake(cursor.getInt(0), cursor.getFloat(1), cursor.getString(2), cursor.getString(3),cursor.getString(4),cursor.getInt(5),cursor.getInt(6)));
+                if (cursor.getFloat(1) < 0)
+                    intakes.add(new Intake(cursor.getInt(0), cursor.getFloat(1), cursor.getString(2), cursor.getString(3), cursor.getString(4), cursor.getInt(5), cursor.getInt(6)));
 
-            }while(cursor.moveToNext());
+            } while (cursor.moveToNext());
         }
         cursor.close();
         return intakes;
     }
 
-    public ArrayList<Intake> getIntakes()
-    {
+    public ArrayList<Intake> getIntakes() {
         ArrayList<Intake> intakes = new ArrayList<>();
         try {
             Cursor cursorIntakes = database.rawQuery("SELECT * FROM intakes", null);
@@ -300,14 +305,13 @@ public class DatabaseHandler{
                 } while (cursorIntakes.moveToNext());
             }
             cursorIntakes.close();
-        } catch(Exception e) {
+        } catch (Exception e) {
             System.out.println("");
         }
         return intakes;
     }
 
-    public ArrayList<Permanent> getPermanents()
-    {
+    public ArrayList<Permanent> getPermanents() {
         ArrayList<Permanent> standing_orders = new ArrayList<>();
         try {
             Cursor cursorIntakes = database.rawQuery("SELECT * FROM permanents", null);
@@ -318,14 +322,13 @@ public class DatabaseHandler{
                 } while (cursorIntakes.moveToNext());
             }
             cursorIntakes.close();
-        } catch(Exception e) {
+        } catch (Exception e) {
             System.out.println("");
         }
         return standing_orders;
     }
 
-    public long addIntake(Intake intake)
-    {
+    public long addIntake(Intake intake) {
         SQLiteStatement sql = database.compileStatement("INSERT INTO intakes (_id, value, date, name, comment, category, payment_opt) VALUES (NULL, ?, ?, ?,?,?,? )");
         sql.bindDouble(1, intake.getValue());
         sql.bindString(2, intake.getDateFormatted());
@@ -342,42 +345,38 @@ public class DatabaseHandler{
         return id;
     }
 
-    public long addPayment(Payment payment)
-    {
+    public long addPayment(Payment payment) {
         SQLiteStatement sql = database.compileStatement("INSERT INTO payment (_id, name) VALUES (NULL, ?)");
-        sql.bindString(1,payment.getName());
+        sql.bindString(1, payment.getName());
         long id = sql.executeInsert();
 
         return id;
     }
 
-    public long addPermanet(Permanent permanent)
-    {
+    public long addPermanet(Permanent permanent) {
         SQLiteStatement sql = database.compileStatement("INSERT INTO permanents (_id, value, start_date, iteration, end_date, name, comment, category, payment_opt, next_exec) " +
                 "VALUES (NULL, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
 
         sql.bindDouble(1, permanent.getValue());
-        sql.bindString(2,permanent.getStartDate());
-        sql.bindString(3,permanent.getIteration());
-        sql.bindString(4,permanent.getEndDate());
-        sql.bindString(5,permanent.getName());
-        sql.bindString(6,permanent.getComment());
-        sql.bindLong(7,permanent.getCategory());
-        sql.bindLong(8,permanent.getPayment_opt());
-        sql.bindString(9,permanent.getNext_exec());
+        sql.bindString(2, permanent.getStartDate());
+        sql.bindString(3, permanent.getIteration());
+        sql.bindString(4, permanent.getEndDate());
+        sql.bindString(5, permanent.getName());
+        sql.bindString(6, permanent.getComment());
+        sql.bindLong(7, permanent.getCategory());
+        sql.bindLong(8, permanent.getPayment_opt());
+        sql.bindString(9, permanent.getNext_exec());
 
         long id = sql.executeInsert();
 
         return id;
     }
 
-    public void removeStandingOrder(String name)
-    {
-      database.delete("permanents", "name = '" + name + "'", null);
+    public void removeStandingOrder(String name) {
+        database.delete("permanents", "name = '" + name + "'", null);
     }
 
-    public long addCategory(Category category)
-    {
+    public long addCategory(Category category) {
         SQLiteStatement sql = database.compileStatement("INSERT INTO categories (_id, name) VALUES (NULL, ?)");
         sql.bindString(1, category.getName());
         long id = sql.executeInsert();
@@ -387,16 +386,14 @@ public class DatabaseHandler{
 
 
     //  ;)
-    public void addCategoryBetter(String name_)
-    {
-        String sql = "INSERT INTO categories (name) VALUES (\"" + name_+ "\");";
+    public void addCategoryBetter(String name_) {
+        String sql = "INSERT INTO categories (name) VALUES (\"" + name_ + "\");";
         database.execSQL(sql);
     }
 
     //  ;)
-    public void addPaymentBetter(String name_)
-    {
-        String sql = "INSERT INTO payment (name) VALUES (\"" + name_+ "\");";
+    public void addPaymentBetter(String name_) {
+        String sql = "INSERT INTO payment (name) VALUES (\"" + name_ + "\");";
         database.execSQL(sql);
     }
 
@@ -419,18 +416,18 @@ public class DatabaseHandler{
 
         List<String> intakes = new ArrayList<String>();
         Cursor cursor = database.rawQuery("SELECT * FROM intakes", null);
-         cursor.moveToFirst();
-         while(!cursor.isAfterLast()){
-             intakes.add(cursor.getString(4));
-             cursor.moveToNext();
-         }
-         cursor.close();
-         System.out.println("INTAKES " + intakes);
+        cursor.moveToFirst();
+        while (!cursor.isAfterLast()) {
+            intakes.add(cursor.getString(4));
+            cursor.moveToNext();
+        }
+        cursor.close();
+        System.out.println("INTAKES " + intakes);
 
         List<String> permanents = new ArrayList<String>();
         Cursor cursor2 = database.rawQuery("SELECT * FROM permanents", null);
         cursor2.moveToFirst();
-        while(!cursor2.isAfterLast()){
+        while (!cursor2.isAfterLast()) {
             permanents.add(cursor2.getString(6));
             cursor2.moveToNext();
 
@@ -441,7 +438,7 @@ public class DatabaseHandler{
         List<String> category = new ArrayList<String>();
         Cursor cursor3 = database.rawQuery("SELECT * FROM categories", null);
         cursor3.moveToFirst();
-        while(!cursor3.isAfterLast()){
+        while (!cursor3.isAfterLast()) {
             category.add(cursor3.getString(1));
             cursor3.moveToNext();
 
@@ -452,7 +449,7 @@ public class DatabaseHandler{
         List<String> payment = new ArrayList<String>();
         Cursor cursor4 = database.rawQuery("SELECT * FROM payment", null);
         cursor4.moveToFirst();
-        while(!cursor4.isAfterLast()){
+        while (!cursor4.isAfterLast()) {
             payment.add(cursor4.getString(1));
             cursor4.moveToNext();
 
@@ -478,7 +475,7 @@ public class DatabaseHandler{
                 } while (cursorIntakes.moveToNext());
             }
             cursorIntakes.close();
-        } catch(Exception e) {
+        } catch (Exception e) {
             System.out.println("");
         }
         return intakes;
--- app/src/main/java/me/finance/finance/EditAccounts.java
+++ app/src/main/java/me/finance/finance/EditAccounts.java
@@ -1,10 +1,16 @@
 package me.finance.finance;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListAdapter;
 import android.widget.TextView;
+
+import me.finance.finance.Model.Payment;
 
 public class EditAccounts extends AppCompatActivity {
 
@@ -14,13 +20,98 @@ public class EditAccounts extends AppCompatActivity {
     private TextView title;
     private ListAdapter adapter;
     private Toolbar myToolbar;
+    private String account_name, new_name;
+    private EditText account_name_input;
+    private Button delete_button, submit_button, cancel_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_accounts);
+        account_name = getIntent().getStringExtra("name");
+        TextView name_view = (TextView)findViewById(R.id.accountName);
+        name_view.setText(account_name);
+        delete_button = findViewById(R.id.delete);
+        submit_button = findViewById(R.id.submit);
+        cancel_button = findViewById(R.id.cancel);
+        account_name_input = findViewById(R.id.accountNameInput);
+
+        final Intent intent = new Intent(this, CategoryActivity.class);
+
+
+        if(getIntent().getStringExtra("status").equals("categories"))
+        {
+            delete_button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    databaseHandler.removeCategory(account_name);
+                    intent.putExtra("settings", "manage categories");
+                    finish();
+                    startActivity(intent);
+                }
+            });
+            submit_button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    new_name = account_name_input.getText().toString();
+                    databaseHandler.updateCategories(new_name, account_name);
+                    intent.putExtra("settings", "manage categories");
+                    finish();
+                    startActivity(intent);
+                }
+            });
+            cancel_button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    intent.putExtra("settings", "manage categories");
+                    finish();
+                    startActivity(intent);
+                }
+            });
+        }
+        else if(getIntent().getStringExtra("status").equals("payments"))
+        {
+            delete_button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    databaseHandler.removePayment(account_name);
+                    intent.putExtra("settings", "manage accounts");
+                    finish();
+                    startActivity(intent);
+                }
+            });
+            submit_button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    new_name = account_name_input.getText().toString();
+                    databaseHandler.updatePayment(new_name, account_name);
+                    intent.putExtra("settings", "manage accounts");
+                    finish();
+                    startActivity(intent);
+                }
+            });
+            cancel_button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    intent.putExtra("settings", "manage accounts");
+                    finish();
+                    startActivity(intent);
+                }
+            });
+        }
+
+
+
+
+
+
+
+
+
+
 
     }
+
 
 
 
